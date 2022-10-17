f15d52a9eb7f7e084fb0063235eb754f93152a9e
EngrAlloy <aniefiokessiet21@gmail.com>
2022-10-17 13:38:05 -0700

    Base 10 task

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints all single digit numbers of base 10
+ * starting from 0 using the putchar function
+ *
+ * Return: Always 0 when successful
+ */
+int main(void)
+{
+	int c;
+
+	c = 0;
+
+	while (c < 10)
+	{
+		putchar(c + '0');
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
