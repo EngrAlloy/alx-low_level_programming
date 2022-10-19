09b78fb8cbd0e9627c33830b58358c48ee13e5dc
EngrAlloy <aniefiokessiet21@gmail.com>
2022-10-19 01:34:44 -0700

    10 times alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,12 +10,16 @@ void print_alphabet_x10(void)
 
 	int count = 0;
 
-	while (count++ < 10)
+	while (count < 10)
 	{
-		for (letter = 'a'; letter <= 'z'; letter++)
+		letter = 'a';
+
+		while (letter <= 'z')
 		{
 			_putchar(letter);
+			letter++;
 		}
 	}
 	_putchar('\n');
+	count++;
 }
