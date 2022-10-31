cfe677a4ded7554dce2d83d8240a30917e83ebf3
EngrAlloy <aniefiokessiet21@gmail.com>
2022-10-31 04:35:42 -0700

    Task 8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13
+ * @str: the string to be encoded
+ * Return: A pointer to the encoded string
+ */
+
+char *rot13(char *str)
+{
+	int x, y;
+
+	char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+	char output[] = "NOPQRSTUVWXYZABCEDFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		for (y = 0; input[y] != '\0'; y++)
+		{
+			if (s[x] == input[y])
+			{
+				s[x] = output[y];
+				break;
+			}
+		}
+	}
+	return (s);
+}
