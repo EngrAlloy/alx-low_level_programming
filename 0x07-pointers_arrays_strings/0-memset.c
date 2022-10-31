9ca7d2bdc403dfebb47c55896dedd6c38c10982d
EngrAlloy <aniefiokessiet21@gmail.com>
2022-10-31 08:59:10 -0700

    Task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -16,8 +16,9 @@ char *_memset(char *s, char b, unsigned int n)
 	unsigned char *memory = s, value = b;
 
 	for (index = 0; index < n; index++)
-
+	{
 		memory[index] = value;
 		return (memory);
+	}
 
 }
