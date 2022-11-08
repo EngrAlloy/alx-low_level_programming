dce8262b7775b1570544b206d61f7b17a318d42a
EngrAlloy <aniefiokessiet21@gmail.com>
2022-11-08 03:16:31 -0800

    Task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,25 +9,35 @@
 
 int _atoi(char *s);
 {
-	int sign = 1;
+	int i = 0;
+	int d = 0;
+	int n = 0;
+	int len = 0;
+	int f = 0;
+	int digit = 0;
 
-	unsigned int num = 0;
+	while (s[len] != '\0')
+		len++;
 
-	while (!('0' <= *s && *s <= '9') && *s != '\0')
+	while (i < len && f == 0)
 	{
-		if (*s == '-')
+		if (s[i] == '-')
+			++d;
 
-			sign *= -1;
-
-		if (*s == '+')
-
-			sign *= +1;
-		s++;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
 	}
-	while ('0' <= *s && *s <= '9' && *s != '\0')
-	{
-		num = (num * 10) + (*s - '0');
-			s++;
-	}
-	return (num * sign);
+	if (f == 0)
+		return (0);
+	return (n);
 }
