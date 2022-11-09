3c0bb9fb5db7a5a8831de6fb39e570da9f21e123
EngrAlloy <aniefiokessiet21@gmail.com>
2022-11-09 07:18:38 -0800

    Task 2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,3 +1,21 @@
 #include <stdio,h>
 #include <stdlib.h>
 
+/**
+ * main - function to print the number of arguments passed
+ * @argc: argc parameter
+ * @argv: an array of a command listed
+ * Return: 0 for success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
+
