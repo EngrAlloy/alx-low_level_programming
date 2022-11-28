3dfebcc7dcfe407494d2991b05bdac25ecf3d425
EngrAlloy <aniefiokessiet21@gmail.com>
2022-11-28 08:22:26 -0800

    Task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "function_pointers.h"
 
 /**
@@ -12,17 +13,11 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i;
+	unsigned int i;
 
-	if (array && cmp)
-	{
-		if (size <= 0)
-			return (-1);
+	if (!array || !action)
+		return;
 
-		for (i = 0; i < size; i++)
-			if (cmp(array[i]))
-				return (i);
-	}
-
-	return (-1);
+	for (i = 0; i < size; i++)
+		action(array[i]);
 }
